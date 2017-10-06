105d508ba8513cddcf619b85e36dcfe533944ebc
Chris Barry <me@chrisb.xyz>
2017-10-06 15:43:31 -0400

    header

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,20 +1,21 @@
 const _ = require('lodash');
 
-
 /**
   Initialises the standard view locals
 */
 exports.initLocals = function (req, res, next) {
   res.locals.navLinks = [
     { label: 'Home', key: 'home', href: '/' },
-    { label: 'Blog', key: 'blog', href: '/blog' },
+    { label: 'About', key: 'about', href: '/about' },
+    { label: 'Collabs', key: 'collabs', href: '/collabs' },
     { label: 'Illustrations', key: 'illustrations', href: '/illustrations' },
-    { label: 'Gallery', key: 'gallery', href: '/gallery' }
+    { label: 'Comics', key: 'comics', href: '/comics' },
+    { label: 'Paperwork', key: 'paperwork', href: '/paperwork' },
+    { label: 'Contact', key: 'contact', href: '/contact' }
   ];
   res.locals.user = req.user;
   next();
 };
-
 
 /**
   Fetches and clears the flashMessages before a view is rendered
@@ -29,7 +30,6 @@ exports.flashMessages = function (req, res, next) {
   res.locals.messages = _.some(flashMessages, msgs => msgs.length) ? flashMessages : false;
   next();
 };
-
 
 /**
   Prevents people from accessing protected pages when they're not signed in
